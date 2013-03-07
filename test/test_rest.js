c4ee24dd1f31396ff4dcb5e5c21ee75889c0cb10
Peter Boyer <peter.b.boyer@gmail.com>
2013-03-07 10:00:08 -0500

    All early REST, auth, and logic tests are passing

--- test/test_rest.js
+++ test/test_rest.js
@@ -5,11 +5,153 @@ var request = require('supertest')
   , request = request('http://localhost:8000');
 
 
-describe('GET /pkg/:name', function(){
+// test user_id
+describe('GET /user_id/:id', function(){
+
+  it('should respond with user object as this id does exist', function(done){
+    request
+      .get('/user_id/5138a97814eb37ee85000003')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        should.equal(res.body.content.username, 'test');
+        should.equal(res.body.success, true);
+        done();
+      });
+  });
+
+  it('should respond with error object as this id does not exist', function(done){
+    request
+      .get('/user_id/5137a7')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        should.equal(res.body.success, false);
+        done();
+      });
+  });
+
+});
+
+// test user_name
+describe('GET /user_name/:name', function(){
+
+  it('should respond with user object as this username does exist', function(done){
+    request
+      .get('/user_name/test')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        should.equal(res.body.content.username, 'test');
+        should.equal(res.body.success, true);
+        done();
+      });
+  });
+
+  it('should respond with error object as this name does not exist', function(done){
+    request
+      .get('/user_name/5137a7')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        should.equal(res.body.success, false);
+        done();
+      });
+  });
+
+});
+
+// test pkg_id
+describe('GET /pkg_id/:id', function(){
 
   it('should respond with error object as this package does not exist', function(done){
     request
-      .get('/pkg/doodoo')
+      .get('/pkg_id/20983')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        should.equal(res.body.success, false);
+        done();
+      });
+  });
+
+  it('should respond with object as this package does exist', function(done){
+    request
+      .get('/pkg_id/5138a97c14eb37ee85000004')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        should.equal(res.body.content.name, 'CoolPackage1362667900345');
+        should.equal(res.body.success, true);
+        done();
+      });
+  });
+
+});
+
+// test pkg_engine
+describe('GET /pkg_engine/:engine', function(){
+
+  it('should respond with a list of packages as \'dynamo\' is a valid engine name', function(done){
+    request
+      .get('/pkg_engine/dynamo')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        should.equal(res.body.success, true);
+        done();
+      });
+  });
+
+  it("should respond with error object as this engine does not exist", function(done){
+    request
+      .get('/pkg_engine/donk')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        should.equal(res.body.success, false);
+        done();
+      });
+  });
+
+});
+
+describe('GET /pkg_engine_name/:engine/:name', function(){
+
+  it('should respond with a single package as this package does exist', function(done){
+    request
+      .get('/pkg_engine_name/dynamo/cool-package')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        console.log(res.body);
+        should.equal(res.body.success, true);
+        should.equal(res.body.content.name, 'cool-package');
+        done();
+      });
+  });
+
+  it("should respond with error object as this engine does not exist", function(done){
+    request
+      .get('/pkg_engine_name/donk/toodle')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
@@ -32,6 +174,7 @@ describe('POST /pkg', function(){
     , engine: 'dynamo'
     , engine_version: '0.3.1'
     , license: 'MIT'
+    , contents: 'some code'
   };
 
   it('should respond with success json', function(done){
@@ -46,25 +189,43 @@ describe('POST /pkg', function(){
       .expect(200)
       .end(function(err, res){
         if (err) return done(err);
-        console.log(res.body);
         should.equal(res.body.success, true);
         done();
       });
-  })
 
-});
+  });
 
-describe('GET /pkg-download/:id', function(){
+  var pkg_data_test = {
+      name: "cool-package"
+    , description: "Cool description"
+    , keywords: ['cool', 'neat', 'fun']
+    , version: '0.0.1'
+    , engine: 'dynamo'
+    , engine_version: '0.3.1'
+    , license: 'MIT'
+    , contents: 'some more code'
+  };
 
-  // TODO: this collects all of the package data including dependencies
-  it('should respond with json', function(done){
+  it('should respond with success json', function(done){
+
     request
-      .get('/pkg/20983')
+      .post('/pkg')
+      .auth('test','e0jlZfJfKS')
+      .set('Content-Type', 'application/json')
+      .send(pkg_data_test)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, done);
-  })
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        should.equal(res.body.success, true);
+        done();
+      });
+      
+  });
+
 });
+
 
 describe('PUT /pkg', function(){
 
@@ -118,6 +279,17 @@ describe('PUT /pkg', function(){
 
 });
 
+describe('GET /pkg_download/:id', function(){
+
+  it('should respond with json', function(done){
+    request
+      .get('/pkg_download/20983')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+
+});
 
 // describe('DELETE /pkg/:id/:version', function(){
 
