2f37ce20dcb19c4846bea571e3a89725939a4121
Peter Boyer <peter.b.boyer@gmail.com>
2013-03-25 14:43:25 -0400

    Oxygen authentication Strategy for passport, oxygen auth for all POST, PUT calls

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ var express = require('express')
   , routes = require('./routes')
   , pkg = require('./routes/package')
   , passport = require('passport')
+  , oxygen = require('./lib/oxygen')
   , user = require('./routes/user')
-  , users = require('./lib/users')
-  , auth = require('./lib/auth')
-  , search = require('./lib/search')
-  , error = require('./lib/error')
-  , request = require('superagent');
+  , users = require('./lib/users');
 
 ////////////////////////
-// Mongo
+// DB
 ////////////////////////
 
   var mongoUri = 'mongodb://localhost/greg-dev';
@@ -24,10 +21,10 @@ var express = require('express')
       } else {
         throw err;
       }
-  })
+  });
 
 ////////////////////////
-// Config
+// Express Config
 ////////////////////////
 
   app.configure(function() {
@@ -40,51 +37,28 @@ var express = require('express')
     app.use(passport.initialize());
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
-
   });
 
 ////////////////////////
-// Routing
+// Routes
 ////////////////////////
 
-  app.get('/pkgs', pkg.all );
-  app.get('/pkg/', pkg.all );
+  var auth_type = "oxygen";
+
+  app.get('/pkgs', passport.authenticate(auth_type, { session: false }), pkg.all );
+  app.get('/pkg', pkg.all );
   app.get('/pkg/:id', pkg.by_id );
   app.get('/pkg_engine/:engine', pkg.by_engine );
   app.get('/pkg_engine/:engine/:name', pkg.by_engine_and_name );
 
-  // package search
   app.get('/pkg_search/', pkg.all );
   app.get('/pkg_search/:query', pkg.search ); 
 
-  app.post('/pkg', passport.authenticate('basic', { session: false }), pkg.add);
-  app.put('/pkg', passport.authenticate('basic', { session: false }), pkg.add_version);
+  app.post('/pkg', passport.authenticate(auth_type, { session: false }), pkg.add);
+  app.put('/pkg', passport.authenticate(auth_type, { session: false }), pkg.add_version);
 
   app.get('/user_name/:name', user.by_name );
   app.get('/user/:id', user.by_id );
-
-  app.get('/auth/', function(req, res) {
-
-    console.log(req.headers.authorization);
-
-    var url = 'https://accounts-staging.autodesk.com/api/oauth/v1/ValidateAuthorization?requestUrl=10.0.0.6&httpMethod=GET&responseFormat=json';
-
-    request
-    .post(url)
-    .set('authorization', req.headers.authorization )
-    .set('Accept', 'application/json')
-    .end(function(res2){
-      console.log(res2);
-      res.send(res2.body);
-    });
-    
-  });
-
-  // for testing purposes, note: no session support
-  app.get('/login', passport.authenticate('basic', { session: false }),
-  function(req, res){
-   res.json({ username: req.user.username });
-  });
 
 ////////////////////////
 // Debug
@@ -96,7 +70,7 @@ var express = require('express')
 // Server
 ////////////////////////
 
-  var port = process.env.PORT || 8000;
+  var port = process.env.PORT || 8080;
   app.listen(port);
   console.log('Starting server on port: ' + port);
 
