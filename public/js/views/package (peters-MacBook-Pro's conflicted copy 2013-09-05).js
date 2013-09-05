151cbc8aea153ea522b3cc6f74a2fd47dc935a25
Peter Boyer <peter.b.boyer@gmail.com>
2013-09-05 18:06:03 -0400

    Removed DS_Store files, added to gitignore, included favicon

---
+++ public/js/views/package (peters-MacBook-Pro's conflicted copy 2013-09-05).js
@@ -0,0 +1,47 @@
+ // js/view/packages.js
+
+  var app = app || {};
+
+  // Package Item View
+  // --------------
+
+  app.PackageView = Backbone.View.extend({
+
+    tagName: 'div',
+
+    className: 'package',
+
+    template: _.template( $('#item-template').html() ),
+
+    events: {
+			 'click .showdeps': 'toggleDeps',
+		   'click': 'expand' 
+    },
+
+    toggleDeps: function(event) {
+		console.log('up')	
+      this.$('.deps-container').toggle();
+      this.$('.full_deps-container').toggle();
+      event.preventDefault();
+			event.stopPropagation();
+    },
+
+    expand: function(event) {
+			console.log('down')
+			this.$('.data-container').toggle();
+		}, 
+
+    initialize: function() {
+
+      this.listenTo(this.model, 'change', this.render);
+
+    },
+
+    render: function() {
+
+      this.$el.html( this.template( this.model.toJSON() ) );
+      return this;
+      
+    }
+
+  });
