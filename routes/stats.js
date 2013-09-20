b234984f49b26d8d080b720d7d9426192de20dc2
Peter Boyer <peter.b.boyer@gmail.com>
2013-09-20 17:49:59 -0400

    Get all stats for an engine

--- routes/stats.js
+++ routes/stats.js
@@ -14,13 +14,30 @@ exports.by_engine_and_query = function(req, res) {
 		return res.send(404, error.fail("No such statistic"));
 	}
  
-  stats[query_type]( engine, limit, false, function(err, pkgs){
+  stats[query_type]( engine, limit, function(err, pkgs){
 
     if ( err || !pkgs || pkgs.length === 0 ){
       return res.send( 404, error.fail("No results") );
     }
 
     return res.send( error.success_with_content('Found stats', pkgs) );
+
+  });
+
+}
+
+exports.by_engine = function(req, res) {
+
+  var engine = req.params.engine
+    , limit = req.query.limit || exports.DEFAULT_LIMIT;
+ 
+  stats.by_engine( engine, limit, function(err, engine_stats){
+
+    if ( err || !engine_stats ){
+      return res.send( 404, error.fail("No results") );
+    }
+
+    return res.send( error.success_with_content('Found stats', engine_stats) );
 
   });
 
