59e984024b169a4b70342e343c55af606f6b909d
Peter Boyer <peter.b.boyer@gmail.com>
2013-09-24 15:51:08 -0400

    Show metrics in stats ui

--- public/js/models/stat.js
+++ public/js/models/stat.js
@@ -1,14 +1,36 @@
 var app = app || {};
 
 
-  app.Stat = Backbone.Model.extend({
+app.Stat = Backbone.Model.extend({
 
-    idAttribute: "type",
+  idAttribute: "type",
 
-    defaults: {
-      variety: 'package'
-      type: 'most_installed_package',
-      list: ''
-    }
+  defaults: {
+    variety: 'package'
+    type: 'most_installed_package',
+    list: '',
+    metric: ''
+  },
 
-  });
+  stat_map: {
+	  	most_installed_packages : 'downloads'
+	  	, most_recently_updated_packages: 'latest_version_update'
+	  	, most_depended_upon_packages: 'num_dependents'
+	  	, most_commented_upon_packages: 'comments'
+	  	, most_voted_for_authors: 'num_votes_for_maintained_packages'
+	  	, most_installed_authors: 'num_downloads_for_maintained_packages'
+	  	, most_prolific_authors: 'num_maintained_packages'
+	}
+
+  parse: function(stuff) {
+
+  	if (this.stat_map[stuff.type]){
+  		stuff.metric = this.stat_map[stuff.type];
+  	} 
+
+  	console.log(stuff)
+
+    return stuff;
+  }
+
+});
